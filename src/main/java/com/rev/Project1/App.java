bbc0ee5b9f073bbe51228f39593add9da547cd38
Ryan Hodder <i_ryan.h@hotmail.com>
2019-06-27 10:42:06 -0400

    Init commit

---
+++ src/main/java/com/rev/Project1/App.java
@@ -0,0 +1,13 @@
+package com.rev.Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
